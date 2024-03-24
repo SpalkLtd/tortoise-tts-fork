fae6a0395b5e51a3a8c7716a9aba0fce1e4dba97
philip-tian <137120034+philip-tian@users.noreply.github.com>
2024-03-25 12:41:14 +1300

    save

--- tortoise/spalk_api.py
+++ tortoise/spalk_api.py
@@ -8,5 +8,5 @@ if __name__ == '__main__':
     clips_paths = glob.glob(f'{audio_folder}/*.wav')
 
     reference_clips = [utils.audio.load_audio(p, 22050) for p in clips_paths]
-    tts = api.TextToSpeech()
-    pcm_audio = tts.tts_with_preset("Aside from a mishap at the third obstacle, one could say the overall performance was executed quite smoothly", voice_samples=reference_clips, preset='fast')
+    tts = api.TextToSpeech(use_deepspeed=True, kv_cache=True, half=True)
+    pcm_audio = tts.tts_with_preset("Aside from a mishap at the third obstacle, one could say the overall performance was executed quite smoothly", voice_samples=reference_clips, preset='ultra-fast')
