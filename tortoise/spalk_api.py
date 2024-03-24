d9b5bf0eaa1c416a40cba3c0d5dc2a82833d627a
philip-tian <137120034+philip-tian@users.noreply.github.com>
2024-03-25 11:03:11 +1300

    save

---
+++ tortoise/spalk_api.py
@@ -0,0 +1,7 @@
+from tortoise import api
+import utils.audio
+
+if __name__ == '__main__':
+    reference_clips = [utils.audio.load_audio(p, 22050) for p in clips_paths]
+    tts = api.TextToSpeech()
+    pcm_audio = tts.tts_with_preset("your text here", voice_samples=reference_clips, preset='fast')
